784875f96f8378f8e3743c45888e5f4cd6110335
Henrique <henrique@adify.com.br>
2026-01-26 16:17:12 -0300

    vite config

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,10 @@ export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
     port: 8080,
+    allowedHosts: [
+      "henriquearsego.com.br",
+      "www.henriquearsego.com.br"
+    ],
     hmr: {
       overlay: false,
     },
