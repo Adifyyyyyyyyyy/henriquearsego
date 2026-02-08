ff1bd15dffd663ac209f2fcd9e2989920bcf1c5f
Henrique <henrique@adify.com.br>
2026-02-07 21:14:01 -0300

    vite

--- vite.config.ts
+++ vite.config.ts
@@ -4,22 +4,11 @@ import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  server: {
-    host: "::",
-    port: 8080,
-    allowedHosts: [
-      "henriquearsego.com.br",
-      "www.henriquearsego.com.br"
-    ],
-    hmr: {
-      overlay: false,
-    },
-  },
-  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+export default defineConfig({
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-}));
+});
